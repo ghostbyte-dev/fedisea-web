18a100793eeea9df142f681429cdf99fd8f4991a
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2026-03-20 00:56:23 +0100

    Add plausible script

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PlausibleProvider from "next-plausible";
 import { type ReactNode, useState } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PlausibleProvider
+      domain="socialfolio.me"
+      selfHosted
+      trackOutboundLinks
+      hash
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </PlausibleProvider>
   );
 }
